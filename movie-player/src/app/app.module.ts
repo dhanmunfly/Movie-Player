aac16131bf0a1e993586501181c3c5258c19c97b
dhananjay yadav <dhananjayy10@gmail.com>
2019-09-08 23:35:45 +0530

    customUppercase pipe added

--- movie-player/src/app/app.module.ts
+++ movie-player/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { PlayerComponent } from './player/player.component';
 import { PlaylistComponent } from './playlist/playlist.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SafeUrlPipe } from './safeurl.pipe';
+import { CustomUppercasePipe } from './custom-uppercase.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlayerComponent,
     PlaylistComponent,
-    SafeUrlPipe
+    SafeUrlPipe,
+    CustomUppercasePipe
   ],
   imports: [
     BrowserModule,
